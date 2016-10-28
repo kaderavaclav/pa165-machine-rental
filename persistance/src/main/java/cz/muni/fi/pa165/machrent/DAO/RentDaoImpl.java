54a4812701a7e351c6a2b1640ddbbb914a72c2c2
benusp <petobenus@gmail.com>
2016-10-28 15:29:52 +0200

    #5 fixing rent entity

--- persistance/src/main/java/cz/muni/fi/pa165/machrent/DAO/RentDaoImpl.java
+++ persistance/src/main/java/cz/muni/fi/pa165/machrent/DAO/RentDaoImpl.java
@@ -5,7 +5,7 @@
  */
 package cz.muni.fi.pa165.machrent.DAO;
 
-import cz.muni.fi.pa165.machrent.Entities.RentEntity;
+import cz.muni.fi.pa165.machrent.Entities.Rent;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -16,25 +16,34 @@ import org.springframework.stereotype.Repository;
  * @author Peter Benus
  */
 @Repository
-public class Rent implements RentDAO{
+public class RentDaoImpl implements RentDAO{
 
     @PersistenceContext
 	private EntityManager em; 
     
-    public void create(RentEntity r) {
+    @Override
+    public void create(Rent r) {
         em.persist(r);
     }
 
-    public void delete(RentEntity r) {
+    @Override
+    public void delete(Rent r) {
         em.remove(r);
     }
 
-    public void update(RentEntity r) {
+    @Override
+    public void update(Rent r) {
         em.merge(r);
     }
 
-    public List<RentEntity> get(RentEntity r) {  
-        return em.createQuery("SELECT r FROM rent r", RentEntity.class).getResultList();
+    @Override
+    public Rent findById(Long id) {
+        return em.find (Rent.class, id);
+    }
+
+    @Override
+    public List<Rent> findAll(Rent r) {
+        return em.createQuery("SELECT r FROM Rent r", Rent.class).getResultList();
     }
 
     
--- persistance/src/main/java/cz/muni/fi/pa165/machrent/Entities/Rent.java
+++ persistance/src/main/java/cz/muni/fi/pa165/machrent/Entities/Rent.java
@@ -5,29 +5,30 @@
  */
 package cz.muni.fi.pa165.machrent.Entities;
 
+import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  *
  * @author Peter Benus
  */
-public class RentEntity {
+@Entity
+@Table (name = "Rent")
+public class Rent implements Serializable {
 
     private Machine machine;
     private User customer;
     private String note;
     private User employee;
     private Date dateStart;
-    private Date dateEnd;
-
-    
+    private Date dateEnd;    
     private Date dateCreated;
 
-    
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -91,5 +92,41 @@ public class RentEntity {
     public void setDateCreated(Date dateCreated) {
         this.dateCreated = dateCreated;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + (this.machine != null ? this.machine.hashCode() : 0);
+        hash = 97 * hash + (this.customer != null ? this.customer.hashCode() : 0);
+        hash = 97 * hash + (this.dateCreated != null ? this.dateCreated.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Rent other = (Rent) obj;
+        if (this.machine != other.machine && (this.machine == null || !this.machine.equals(other.machine))) {
+            return false;
+        }
+        if (this.customer != other.customer && (this.customer == null || !this.customer.equals(other.customer))) {
+            return false;
+        }
+        if (this.dateCreated != other.dateCreated && (this.dateCreated == null || !this.dateCreated.equals(other.dateCreated))) {
+            return false;
+        }
+        return true;
+    }
+
+    
+    
     
 }
