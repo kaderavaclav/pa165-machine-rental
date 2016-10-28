35c92677dbdf31c47c4709c4182ab3a0bfc81b78
Zuzana Schwarzova <zuzka.schwarzova@gmail.com>
2016-10-28 14:34:27 +0200

    fixing stuff

--- persistance/src/main/java/cz/muni/fi/pa165/machrent/entity/SampleEntity.java
+++
@@ -1,7 +0,0 @@
-package cz.muni.fi.pa165.machrent.dao.Entities;
-
-/**
- * Created by vaclav.kadera on 27-Oct-16.
- */
-public class SampleEntity {
-}
--- persistance/src/main/java/cz/muni/fi/pa165/machrent/repository/Repository.java
+++
@@ -1,8 +0,0 @@
-package cz.muni.fi.pa165.machrent.repository;
-
-/**
- * Created by vaclav.kadera on 27-Oct-16.
- */
-public class Repository<IEntityDao> {
-
-}
