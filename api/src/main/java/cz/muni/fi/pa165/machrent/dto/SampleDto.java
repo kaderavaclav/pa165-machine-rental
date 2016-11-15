14cd6276da97081b4ae0b5f75215befb71d4a65c
kaderavaclav <vaclav.kadera@ictcapital.cz>
2016-11-15 11:36:21 +0100

    API & Service project initialization + groupId bugfix

---
+++ api/src/main/java/cz/muni/fi/pa165/machrent/dto/SampleDto.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.machrent.dto;
+
+/**
+ * Created by vaclav.kadera on 15-Nov-16.
+ */
+public class SampleDto {
+}
---
+++ service/src/main/java/cz/muni/fi/pa165/machrent/SampleService.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.machrent;
+
+/**
+ * Created by vaclav.kadera on 15-Nov-16.
+ */
+public class SampleService {
+}
