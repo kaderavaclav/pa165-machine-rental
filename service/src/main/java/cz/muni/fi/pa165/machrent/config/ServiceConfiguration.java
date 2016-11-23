fc61f31286927a19aef3846b9257e93fed4ded26
kaderavaclav <vaclav.kadera@ictcapital.cz>
2016-11-23 19:34:28 +0100

    RentalUser Service & Facade testy

--- service/src/main/java/cz/muni/fi/pa165/machrent/config/ServiceConfiguration.java
+++ service/src/main/java/cz/muni/fi/pa165/machrent/config/ServiceConfiguration.java
@@ -1,8 +1,11 @@
 package cz.muni.fi.pa165.machrent.config;
 
 import cz.muni.fi.pa165.machrent.PersistenceApplicationContext;
+import cz.muni.fi.pa165.machrent.dto.RentalUserDto;
+import cz.muni.fi.pa165.machrent.entities.RentalUser;
 import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
+import org.dozer.loader.api.BeanMappingBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,11 +16,12 @@ import org.springframework.context.annotation.Import;
  */
 @Configuration
 @Import(PersistenceApplicationContext.class)
-@ComponentScan(basePackages = {"cz.muni.fi.pa165.machrent", "cz.muni.fi.pa165.machrent.facade"})
+@ComponentScan(basePackages = {"cz.muni.fi.pa165.machrent"})
 public class ServiceConfiguration {
 
     @Bean
     public Mapper dozer(){
         return new DozerBeanMapper();
     }
+
 }
