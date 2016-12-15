96081c46ecdad5472d48ee8b9b61f30a16abfc09
zuz-schwarzova <zuzka.schwarzova@gmail.com>
2016-12-15 13:02:14 +0100

    web module structure

---
+++ web/src/main/java/cz/muni/fi/pa165/machrent/config/Nothing.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.machrent.config;
+
+/**
+ * Created by Falka on 15. 12. 2016.
+ */
+public class Nothing {
+}
---
+++ web/src/main/java/cz/muni/fi/pa165/machrent/controllers/RevisionController.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.machrent.controllers;
+
+/**
+ * Created by Falka on 15. 12. 2016.
+ */
+public class RevisionController {
+}
